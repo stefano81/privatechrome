52c4e212536d4609e8e0a1d8dac1df6a9ae25030
Stefano Braghin <stefano.braghin@gmail.com>
2013-07-25 14:55:58 +0100

    ok, for now it is ok

--- src/main/chrome/main.js
+++ src/main/chrome/main.js
@@ -29,19 +29,20 @@ function decrypt(schema, ciphertext, password) {
 }
 
 function resetDOM(doc) {
-    for (i = 0; i < doc.attributes.length; ++i) {
-
+    var i = 0;
+    while (0 != doc.childNodes.length) {
+	console.log((i++) + " removing " + doc.childNodes[0].outerHTML);
+	doc.removeChild(doc.childNodes[0]);
     }
-    for (i = 0; i < doc.attribtues.length;
 }
 
-function restoreDecyptedDOM(doc, dec) {
+function restoreDecryptedDOM(doc, dec) {
     for (i = 0; i < dec.attributes.length; ++i) {
-	var attr = element.attributes[i];
+	var attr = dec.attributes[i];
 	doc.setAttribute(attr.name, attr.value);
     }
     for (i = 0; i < dec.childNodes.length; ++i) {
-	    doc.appendChild(element.childNodes[i]);
+	    doc.appendChild(dec.childNodes[i]);
     }
 }
 
@@ -60,36 +61,36 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
 
 	var plain = decrypt(schema, oHtml.textContent.trim(), message.password);
 
-	var element = document.createElement('html');
+	/*var element = document.createElement('html');
 	element.innerHTML = plain;
 
 	resetDOM(document.documentElement);
 
-	restoreDecryptedDOM(document.documentElement, element);
+	restoreDecryptedDOM(document.documentElement, element);*/
 	
-	//oHtml.innerHTML = plain;
+	oHtml.innerHTML = plain;
 
-	//var script = document.createElement('script');
+	var script = document.createElement('script');
 	/*script.textContent = [ "function(){",
 			       "alert('something');",
 			       "var myevent = new Event('DOMContentLoaded', {bubble : true, cancelable: true});",
 			       "document.documentElement.dispatchEvent(myevent);",
 			       "}();"
 	].join('\n');*/
-	/*script.textContent = [
+	script.textContent = [
 	    //"document.documentElement.dispatchEvent(new Event('DOMContentLoaded', {bubbles : true, cancelable : true}));",
 	    "document.addEventListener('load', function() {",
 	    "alert('loaded')",
 	    "});",
-	    "var evt = new Event('load', {bubbles : true, cabcelable : true});",
+	    "var evt = new Event('load', {bubbles : true, cancelable : true});",
 	    //"var evt = new Event('DOMContentLoaded', {bubbles : true, cabcelable : true});",
-	    "document.dispatchEvent(evt);"
+	    "document.body.dispatchEvent(evt);"
 	].join('\n');
 	var doc = (document.head || document.documentElement);
 	doc.insertBefore(script, doc.childNodes[0]);
-	script.parentNode.removeChild(script);*/
+	script.parentNode.removeChild(script);
 
-	var response = element.outerHTML;
+	var response = "ok"element.outerHTML;
     }
 
     sendResponse(response);
